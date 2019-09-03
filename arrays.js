d2dad7b5a2f21561530927336842597c06124e27
John Ragbir <johnragbir@gmail.com>
2019-09-03 15:53:58 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+
+  var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"
+  ];
+
+
+
+function addElementToBeginningOfArray (chocolateBars)
+    
+    console.log(chocolateBars[1])
+  
+  
+  
+  function destructivelyAddElementToBeginningOfArray (chocolateBars) chocolateBars.unshift("snickers")
+
